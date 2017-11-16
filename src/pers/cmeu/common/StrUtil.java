2d7467d02af4d55fbf9b0f3726a50fabee78d6ad
shenzhenMirren <690389491@qq.com>
2017-11-16 10:22:48 +0800

    修复类似表名t_user帕斯卡转换不正确

--- src/pers/cmeu/common/StrUtil.java
+++ src/pers/cmeu/common/StrUtil.java
@@ -65,7 +65,7 @@ public class StrUtil {
 	 * @return
 	 */
 	public static String fristToUpCaseLaterToLoCase(String str) {
-		if (str != null && str.length() > 1) {
+		if (str != null && str.length() > 0) {
 			str = (str.substring(0, 1).toUpperCase()) + (str.substring(1).toLowerCase());
 		}
 		return str;
@@ -78,7 +78,7 @@ public class StrUtil {
 	 * @return
 	 */
 	public static String fristToLoCaseLaterToUpCase(String str) {
-		if (str != null && str.length() > 1) {
+		if (str != null && str.length() > 0) {
 			str = (str.substring(0, 1).toLowerCase()) + (str.substring(1).toUpperCase());
 
 		}
@@ -92,7 +92,7 @@ public class StrUtil {
 	 * @return
 	 */
 	public static String fristToUpCase(String str) {
-		if (str != null && str.length() > 1) {
+		if (str != null && str.length() > 0) {
 			str = str.substring(0, 1).toUpperCase() + str.substring(1);
 		}
 		return str;
@@ -105,7 +105,7 @@ public class StrUtil {
 	 * @return
 	 */
 	public static String fristToLoCase(String str) {
-		if (str != null && str.length() > 1) {
+		if (str != null && str.length() > 0) {
 			str = str.substring(0, 1).toLowerCase() + str.substring(1);
 		}
 		return str;
